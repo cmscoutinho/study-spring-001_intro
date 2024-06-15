81832368817b5d2716cc975f6d1859b31a7e058f
Claudio Coutinho <ccoutinho.ti@gmail.com>
2024-06-15 12:33:24 -0300

    01_10: add DataConverter and IDataConverter

---
+++ screenmatch/src/main/java/br/com/alura/screenmatch/service/DataConverter.java
@@ -0,0 +1,17 @@
+package br.com.alura.screenmatch.service;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class DataConverter implements IDataConverter{
+    private ObjectMapper mapper = new ObjectMapper();
+
+    @Override
+    public <T> T getData(String json, Class<T> TClass) {
+        try {
+            return mapper.readValue(json, TClass);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
