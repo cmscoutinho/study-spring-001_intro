06679e9ac87a9d35b488eefbe88cb9876f3192ae
Claudio Coutinho <ccoutinho.ti@gmail.com>
2024-06-18 10:58:10 -0300

    03_02: stream and lambda example

--- screenmatch/src/main/java/br/com/alura/screenmatch/main/Main.java
+++ screenmatch/src/main/java/br/com/alura/screenmatch/main/Main.java
@@ -6,6 +6,7 @@ import br.com.alura.screenmatch.service.ApiConsumer;
 import br.com.alura.screenmatch.service.DataConverter;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 
@@ -17,25 +18,35 @@ public class Main {
     private final String BASE_URL = "https://www.omdbapi.com/?t=";
     private final String API_KEY = "&apikey=337e4e55";
     public void showMenu() {
-        System.out.print("Which title are you searching?: ");
-        var title = scanner.nextLine();
-        var fullUrl = BASE_URL + title.replace(" ", "+") + API_KEY;
-        var json = consumer.getData(fullUrl);
+//        System.out.print("Which title are you searching?: ");
+//        var title = scanner.nextLine();
+//        var fullUrl = BASE_URL + title.replace(" ", "+") + API_KEY;
+//        var json = consumer.getData(fullUrl);
+//
+//        SeriesData series = converter.getData(json, SeriesData.class);
+//        System.out.println(series);
+//
+////        EpisodeData episode = converter.getData(json, EpisodeData.class);
+//
+//        List<SeasonData> seasons = new ArrayList<>();
+//
+//        for (int i = 1; i <= series.seasons(); i++) {
+//            json = consumer.getData("https://www.omdbapi.com/?t=" + title.replace(" ", "+") + "&season=" + i + API_KEY);
+//            SeasonData season = converter.getData(json, SeasonData.class);
+//            seasons.add(season);
+//        }
+////        seasons.forEach(System.out::println);
+//
+//        seasons.forEach(s -> s.episodes().forEach(e -> System.out.println(e.title())));
 
-        SeriesData series = converter.getData(json, SeriesData.class);
-        System.out.println(series);
+        // Stream and lambda example
+        List<String> names = Arrays.asList("Claudio","Carol","Cecília","Dalva","Brena","Ana Carla","Alcir","Edna","Camila","Luiz");
 
-//        EpisodeData episode = converter.getData(json, EpisodeData.class);
-
-        List<SeasonData> seasons = new ArrayList<>();
-
-        for (int i = 1; i <= series.seasons(); i++) {
-            json = consumer.getData("https://www.omdbapi.com/?t=" + title.replace(" ", "+") + "&season=" + i + API_KEY);
-            SeasonData season = converter.getData(json, SeasonData.class);
-            seasons.add(season);
-        }
-//        seasons.forEach(System.out::println);
-
-        seasons.forEach(s -> s.episodes().forEach(e -> System.out.println(e.title())));
+        names.stream()
+                .sorted()
+                .filter(n -> !n.endsWith("o"))
+                .map(n -> n.toUpperCase())
+                .limit(4)
+                .forEach(System.out::println);
     }
 }
